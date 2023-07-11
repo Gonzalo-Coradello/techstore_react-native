cc8afa852c3d9000d9c8a30a7cc42453c8bc11ef
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-11 20:16:44 -0300

    add styles for tablet using useWindowDimension hook

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -18,8 +18,28 @@ export const styles = StyleSheet.create({
     shadowRadius: 2.62,
     elevation: 4,
   },
+  tabletContainer: {
+    height: 100,
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: COLORS.secondary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
   title: {
     fontSize: 20,
+    color: COLORS.text,
+    fontFamily: FONTS.bold,
+  },
+  tabletTitle: {
+    fontSize: 35,
     color: COLORS.text,
     fontFamily: FONTS.bold,
   },
