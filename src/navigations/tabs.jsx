dd69b0c78c243a7991a49b41fee2f5f97a034ded
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-18 20:03:17 -0300

    fix icon size

--- src/navigations/tabs.jsx
+++ src/navigations/tabs.jsx
@@ -32,8 +32,8 @@ const TabsNavigator = () => {
         component={ShopNavigator}
         options={{
           tabBarLabel: 'Shop',
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons name={focused ? 'home' : 'home-outline'} size={20} color={color} />
           ),
         }}
       />
@@ -42,8 +42,8 @@ const TabsNavigator = () => {
         component={CartNavigator}
         options={{
           tabBarLabel: 'Cart',
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons name={focused ? 'cart' : 'cart-outline'} size={size} color={color} />
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons name={focused ? 'cart' : 'cart-outline'} size={20} color={color} />
           ),
           tabBarBadge: 2,
           tabBarBadgeStyle: {
@@ -59,12 +59,8 @@ const TabsNavigator = () => {
         component={OrdersNavigator}
         options={{
           tabBarLabel: 'Orders',
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons
-              name={focused ? 'file-tray' : 'file-tray-outline'}
-              size={size}
-              color={color}
-            />
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons name={focused ? 'file-tray' : 'file-tray-outline'} size={20} color={color} />
           ),
         }}
       />
