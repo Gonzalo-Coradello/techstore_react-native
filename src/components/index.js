7fbe15329eddd7cc6944b4db26b1ff1705552f9f
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-19 07:17:48 -0300

    create cart screen

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './header';
 export { default as CategoryItem } from './categories/item';
 export { default as Input } from './input';
+export { default as CartItem } from './cart/item';
